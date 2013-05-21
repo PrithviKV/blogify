c321be45110a3c646bf8ab7b3cdac58016258d14
PrithviKV <kvprithvi5@gmail.com>
2013-05-21 17:53:21 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogify::Application.config.secret_token = '6bee3c9629a5c26a4ca8d0cb0047250b179401f18a66e6ec51ca09ad171bba65e1cdcae59cbfb570de3dff7c42508033d8739a965c8ee468a051e6bbde44abac'
